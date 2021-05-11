2773551fc90ebbc4dcf0e3f8d9a89ec39f300273
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-11 16:47:35 +0900

    [ADD] CGV 프로젝트 초기 세팅

---
+++ CGV/CGV/Source/VCs/MovieChartVC.swift
@@ -0,0 +1,29 @@
+//
+//  MovieChartVC.swift
+//  CGV
+//
+//  Created by SHIN YOON AH on 2021/05/11.
+//
+
+import UIKit
+
+class MovieChartVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
