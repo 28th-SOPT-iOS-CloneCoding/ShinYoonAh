dd87c9e68479fc68bcbd9f94627bece6bfd969c2
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-11 17:19:15 +0900

    [FEAT] connect NavigationController

---
+++ CGV/CGV/Source/VCs/HomeVC.swift
@@ -0,0 +1,23 @@
+//
+//  HomeVC.swift
+//  CGV
+//
+//  Created by SHIN YOON AH on 2021/05/11.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func touchUpMore(_ sender: Any) {
+        guard let dvc = UIStoryboard(name: "MovieChart", bundle: nil).instantiateViewController(identifier: "MovieChartVC") as? MovieChartVC else {
+            return
+        }
+        navigationController?.pushViewController(dvc, animated: true)
+    }
+}
