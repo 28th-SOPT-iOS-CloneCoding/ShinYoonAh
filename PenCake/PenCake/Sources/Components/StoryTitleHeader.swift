6227f66dbce1c52e92bcb83b787bbc26f4e3834e
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-30 22:51:32 +0900

    [ADD] SnapKit 라이브러리 추가

--- PenCake/PenCake/Sources/Components/StoryTitleHeader.swift
+++ PenCake/PenCake/Sources/Components/StoryTitleHeader.swift
@@ -8,6 +8,25 @@
 import UIKit
 
 class StoryTitleHeader: UIView {
+    private var titleButton: UIButton = {
+        let button = UIButton()
+        button.titleLabel?.font = .myBoldSystemFont(ofSize: 17)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
+    private var subTitleButton: UIButton = {
+        let button = UIButton()
+        button.titleLabel?.font = .myRegularSystemFont(ofSize: 13)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
+    private var bottomLine: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        return view
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -18,10 +37,6 @@ class StoryTitleHeader: UIView {
     }
     
     override func layoutSubviews() {
-        
-    }
-    
-    private func setupConfigure() {
         
     }
 }
