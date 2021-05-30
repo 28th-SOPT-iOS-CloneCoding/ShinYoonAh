c6cd04b6da94d471d48f2bf3b21a3cb26c219156
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-30 21:20:15 +0900

    [FEAT] Page 기능 구현

--- PenCake/PenCake/Sources/Controllers/StoryPageVC.swift
+++ PenCake/PenCake/Sources/Controllers/StoryPageVC.swift
@@ -7,23 +7,66 @@
 
 import UIKit
 
-class StoryPageVC: UIViewController {
+class StoryPageVC: UIPageViewController {
+    var completeHandler: ((Int) -> ())?
+    
+    var viewsList: [UIViewController] = {
+        let storyboard = UIStoryboard(name: "StoryPage", bundle: nil)
+        let mainVC = storyboard.instantiateViewController(withIdentifier: "MainStoryVC")
+        let createVC = storyboard.instantiateViewController(withIdentifier: "CreateStoryVC")
+        
+        return [mainVC, createVC]
+    }()
+    
+    var currentIndex: Int {
+        guard let vc = viewControllers?.first else { return 0 }
+        return viewsList.firstIndex(of: vc) ?? 0
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        pageInit()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func pageInit() {
+        self.dataSource = self
+        self.delegate = self
+        
+        if let firstVC = viewsList.first {
+            self.setViewControllers([firstVC],
+                                    direction: .forward,
+                                    animated: true,
+                                    completion: nil)
+        }
     }
-    */
+    
+    func setViewControllersFromIndex(index: Int) {
+        if index < 0 && index >= viewsList.count { return }
+        self.setViewControllers([viewsList[index]], direction: .forward, animated: true, completion: nil)
+        completeHandler?(currentIndex)
+    }
+}
 
+extension StoryPageVC: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let index = viewsList.firstIndex(of: viewController) else { return nil }
+        let previousIndex = index - 1
+        if previousIndex < 0 { return nil }
+        return viewsList[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = viewsList.firstIndex(of: viewController) else { return nil }
+        let nextIndex = index + 1
+        if nextIndex == viewsList.count { return nil }
+        return viewsList[nextIndex]
+    }
+}
+
+extension StoryPageVC: UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if completed {
+            completeHandler?(currentIndex)
+        }
+    }
 }
