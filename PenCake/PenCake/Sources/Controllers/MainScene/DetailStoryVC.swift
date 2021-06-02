938b6fa7bbf7f77b94b6ba98c286473f9dbac393
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-06-03 02:03:03 +0900

    [FEAT] TableViewCell View backButton 구현

--- PenCake/PenCake/Sources/Controllers/MainScene/DetailStoryVC.swift
+++ PenCake/PenCake/Sources/Controllers/MainScene/DetailStoryVC.swift
@@ -6,8 +6,23 @@
 //
 
 import UIKit
+import SnapKit
 
 class DetailStoryVC: UIViewController {
+    lazy private var backButton: UIButton = {
+        let button = UIButton()
+        let action = UIAction { _ in
+            self.navigationController?.popViewController(animated: true)
+        }
+        button.addAction(action, for: .touchUpInside)
+        button.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        button.setPreferredSymbolConfiguration(.init(pointSize: 18,
+                                                    weight: .light,
+                                                    scale: .large),
+                                               forImageIn: .normal)
+        button.tintColor = .lightGray
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,10 +30,20 @@ class DetailStoryVC: UIViewController {
         setupConfigure()
     }
     
+    override func viewWillLayoutSubviews() {
+        backButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(10)
+            make.leading.equalToSuperview().inset(10)
+            make.width.height.equalTo(40)
+        }
+    }
+    
     private func setupConfigure() {
         view.layer.masksToBounds = false
         view.layer.shadowOpacity = 0.8
         view.layer.shadowOffset = CGSize(width: 0, height: 0)
         view.layer.shadowRadius = 8
+        
+        view.addSubview(backButton)
     }
 }
