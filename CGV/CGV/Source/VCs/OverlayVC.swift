6624b9e9397ca9b6821e912499bb2cb4c29d48a3
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-06-04 16:21:25 +0900

    [CHORE] Code Convention 적용해서 함수 정렬

--- CGV/CGV/Source/VCs/OverlayVC.swift
+++ CGV/CGV/Source/VCs/OverlayVC.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class OverlayVC: UIViewController {
+    // MARK: - Properties
     @IBOutlet weak var swipeButton: UIView!
     @IBOutlet weak var backView: UIView!
     @IBOutlet weak var bookingTableView: UITableView!
@@ -17,6 +18,7 @@ class OverlayVC: UIViewController {
     private var positionCount = 0
     private var viewTranslation = CGPoint(x: 0, y: 0)
     
+    // MARK: - Life Cycle
     override func viewWillAppear(_ animated: Bool) {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.45, execute: {
             self.view.backgroundColor = UIColor.black.withAlphaComponent(0.3)
@@ -25,11 +27,106 @@ class OverlayVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUI()
-        setNotification()
+        setupConfigure()
+        setupNotification()
+    }
+    
+    // MARK: - Setting Functions
+    private func setupConfigure() {
+        backView.layer.cornerRadius = 20
+        swipeButton.layer.cornerRadius = 3
+        
+        tableViewSetting()
+        setupTouchGesture()
+        setupActions()
+    }
+    
+    private func tableViewSetting() {
+        bookingTableView.delegate = self
+        bookingTableView.dataSource = self
+        
+        let selectNib = UINib(nibName: "SelectTheaterTVC", bundle: nil)
+        let dateNib = UINib(nibName: "DateTheaterTVC", bundle: nil)
+        bookingTableView.register(selectNib, forCellReuseIdentifier: SelectTheaterTVC.identifier)
+        bookingTableView.register(dateNib, forCellReuseIdentifier: DateTheaterTVC.identifier)
+    }
+    
+    
+    private func setupTouchGesture() {
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handleDismiss)))
+    }
+    
+    private func setupActions() {
+        let xmarkAction = UIAction { _ in
+            print("dismiss")
+            
+            self.view.backgroundColor = UIColor.clear
+            self.dismiss(animated: true, completion: nil)
+        }
+        xmarkButton.addAction(xmarkAction, for: .touchUpInside)
+    }
+    
+    private func setupNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(cellIncrease), name: NSNotification.Name("increaseCell"), object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(cellDecrease), name: NSNotification.Name("decreaseCell"), object: nil)
+    }
+    
+    // MARK: - @Objc Functions
+    @objc
+    func cellIncrease(_ notification: Notification) {
+        print("increase")
+        isClicked = true
+        positionCount = notification.object as! Int
+        bookingTableView.beginUpdates()
+        bookingTableView.endUpdates()
+    }
+    
+    @objc
+    func cellDecrease() {
+        print("decrease")
+        isClicked = false
+        bookingTableView.beginUpdates()
+        bookingTableView.endUpdates()
+    }
+    
+    @objc
+    func handleDismiss(sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .changed:
+            viewTranslation = sender.translation(in: view)
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                if self.viewTranslation.y >= 0 {
+                    var colorAlpha = 0.3 - (self.viewTranslation.y / 1000)
+                    
+                    if colorAlpha < 0.1 {
+                        colorAlpha = 0.1
+                    }
+                    self.view.backgroundColor = UIColor.black.withAlphaComponent(colorAlpha
+                    )
+                    
+                    self.swipeButton.transform = CGAffineTransform(translationX: 0, y: self.viewTranslation.y)
+                    self.backView.transform = CGAffineTransform(translationX: 0, y: self.viewTranslation.y)
+                }
+            })
+        case .ended:
+            if viewTranslation.y < 200 {
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                    self.view.backgroundColor = UIColor.black.withAlphaComponent(0.3)
+                    self.swipeButton.transform = .identity
+                    self.backView.transform = .identity
+                })
+            } else {
+                self.view.backgroundColor = UIColor.clear
+                dismiss(animated: true, completion: nil)
+            }
+        default:
+            break
+        }
     }
 }
 
+// MARK: - UITableViewDataSource
 extension OverlayVC: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 2
@@ -55,6 +152,7 @@ extension OverlayVC: UITableViewDataSource {
     }
 }
 
+// MARK: - UITableViewDelegate
 extension OverlayVC: UITableViewDelegate {
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         if section == 0 {
@@ -103,35 +201,7 @@ extension OverlayVC: UITableViewDelegate {
 }
 
 extension OverlayVC {
-    private func setUI() {
-        setTableView()
-        setView()
-        setButton()
-        setGesture()
-    }
-    
-    private func setTableView() {
-        bookingTableView.delegate = self
-        bookingTableView.dataSource = self
-        
-        let selectNib = UINib(nibName: "SelectTheaterTVC", bundle: nil)
-        let dateNib = UINib(nibName: "DateTheaterTVC", bundle: nil)
-        bookingTableView.register(selectNib, forCellReuseIdentifier: SelectTheaterTVC.identifier)
-        bookingTableView.register(dateNib, forCellReuseIdentifier: DateTheaterTVC.identifier)
-    }
-    
-    private func setView() {
-        backView.layer.cornerRadius = 20
-        swipeButton.layer.cornerRadius = 3
-    }
-    
-    private func setGesture() {
-        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handleDismiss)))
-    }
-    
-    private func setButton() {
-        xmarkButton.addTarget(self, action: #selector(touchUpDismiss), for: .touchUpInside)
-    }
+
     
     // MARK: - HeaderView Setting
     private func setSelectTheaterHeader() -> UIView {
@@ -151,82 +221,11 @@ extension OverlayVC {
     }
 }
 
-// MARK: - Action
-extension OverlayVC {
-    @objc
-    func touchUpDismiss() {
-        print("dismiss")
-        
-        self.view.backgroundColor = UIColor.clear
-        dismiss(animated: true, completion: nil)
-    }
-    
-    @objc
-    func handleDismiss(sender: UIPanGestureRecognizer) {
-        switch sender.state {
-        case .changed:
-            viewTranslation = sender.translation(in: view)
-            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-                if self.viewTranslation.y >= 0 {
-                    var colorAlpha = 0.3 - (self.viewTranslation.y / 1000)
-                    
-                    if colorAlpha < 0.1 {
-                        colorAlpha = 0.1
-                    }
-                    self.view.backgroundColor = UIColor.black.withAlphaComponent(colorAlpha
-                    )
-                    
-                    self.swipeButton.transform = CGAffineTransform(translationX: 0, y: self.viewTranslation.y)
-                    self.backView.transform = CGAffineTransform(translationX: 0, y: self.viewTranslation.y)
-                }
-            })
-        case .ended:
-            if viewTranslation.y < 200 {
-                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-                    self.view.backgroundColor = UIColor.black.withAlphaComponent(0.3)
-                    self.swipeButton.transform = .identity
-                    self.backView.transform = .identity
-                })
-            } else {
-                self.view.backgroundColor = UIColor.clear
-                dismiss(animated: true, completion: nil)
-            }
-        default:
-            break
-        }
-    }
-}
-
+// MARK: - ShowUpAlertDelegate
 extension OverlayVC: showupAlertDelegate {
     func showupAlertToLookup(title: String?, content: String) {
         if let title = title {
             makeAlert(title: title, message: content)
         }
-    }
-}
-
-// MARK: Notification
-extension OverlayVC {
-    private func setNotification() {
-        NotificationCenter.default.addObserver(self, selector: #selector(cellIncrease), name: NSNotification.Name("increaseCell"), object: nil)
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(cellDecrease), name: NSNotification.Name("decreaseCell"), object: nil)
-    }
-    
-    @objc
-    func cellIncrease(_ notification: Notification) {
-        print("increase")
-        isClicked = true
-        positionCount = notification.object as! Int
-        bookingTableView.beginUpdates()
-        bookingTableView.endUpdates()
-    }
-    
-    @objc
-    func cellDecrease() {
-        print("decrease")
-        isClicked = false
-        bookingTableView.beginUpdates()
-        bookingTableView.endUpdates()
     }
 }
