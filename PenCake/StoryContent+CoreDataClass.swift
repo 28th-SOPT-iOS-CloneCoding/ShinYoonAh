2f50462f58291f3ab6c7ebe9020d03048b05f1c1
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-06-10 15:25:49 +0900

    [ADD] Core Data Entities

---
+++ PenCake/StoryContent+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  StoryContent+CoreDataClass.swift
+//  PenCake
+//
+//  Created by SHIN YOON AH on 2021/06/10.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(StoryContent)
+public class StoryContent: NSManagedObject {
+
+}
---
+++ PenCake/StoryPage+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  StoryPage+CoreDataClass.swift
+//  PenCake
+//
+//  Created by SHIN YOON AH on 2021/06/10.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(StoryPage)
+public class StoryPage: NSManagedObject {
+
+}
