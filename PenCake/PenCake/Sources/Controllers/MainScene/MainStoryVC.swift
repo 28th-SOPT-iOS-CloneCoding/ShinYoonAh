083320bdf4d447562607122380fe67074f7cd933
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-06-11 14:25:47 +0900

    [FEAT] 게시글 작성 시간 표기 함수 추가

--- PenCake/PenCake/Sources/Controllers/MainScene/MainStoryVC.swift
+++ PenCake/PenCake/Sources/Controllers/MainScene/MainStoryVC.swift
@@ -87,6 +87,44 @@ class MainStoryVC: UIViewController {
         let result = try! context.fetch(fetchRequest)
         return result
     }
+    
+    
+    // 게시글이나 공지같은거 작성할때 서버 시간과 현재 시간을 비교해서
+    // 10분전 , 1시간전 등 작성된 시간을 계산해주는 함수
+    public func slicingDate(date : Date) -> String {
+        var message : String = "" // 최종 결과값 담기위한 변수
+
+        let UTCDate = Date()
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(secondsFromGMT: 32400)
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        let defaultTimeZoneStr = formatter.string(from: UTCDate)
+
+        let format = DateFormatter()
+        format.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        format.locale = Locale(identifier: "ko_KR")
+         // 한국 시각기준으로 측정합니다.
+        
+        let krTime = format.date(from: defaultTimeZoneStr)
+        let articleDate = format.string(from: date)
+        let useTime = Int(krTime!.timeIntervalSince(date))
+        
+        if useTime < 60 {
+            message = "방금 전"
+        } else if useTime < 3600 {
+            message = String(useTime/60) + "분 전"
+        } else if useTime < 86400 {
+            message = String(useTime/3600) + "시간 전"
+        } else {
+            let timeArray = articleDate.components(separatedBy: " ")
+            let dateArray = timeArray[0].components(separatedBy: "-")
+            
+            message = dateArray[1] + "." + dateArray[2]
+        }
+        
+        return message
+    }
 }
 
 extension MainStoryVC: UIGestureRecognizerDelegate {
@@ -106,11 +144,8 @@ extension MainStoryVC: UITableViewDataSource {
         }
         let record = self.list[indexPath.row]
         let title = record.value(forKey: "title") as? String
-        let date = record.value(forKey: "date") as? Date
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MM.dd"
-        let dateString = dateFormatter.string(from: date ?? Date())
+        let date = record.value(forKey: "date") as? Date ?? Date()
+        let dateString = slicingDate(date: date)
         
         cell.titleLabel?.text = title
         cell.dateLabel?.text = "\(String(describing: dateString))"
